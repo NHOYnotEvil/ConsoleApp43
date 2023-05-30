ddb19723cac19495b1cf5baaaedbfdca5b591eca
NHOY <NHOY@2210DR000919>
2023-05-31 01:17:30 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp43/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApp43
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tryCount = 5;
+            string commandCloseProgram = "exit";
+            string userInput;
+
+            for (int i = 0; i < tryCount; i++)
+            {
+                Console.WriteLine("Команда для зыкрытия программы - \"" + commandCloseProgram + "\"");
+                Console.Write("Введите команду для закрытия программы:");
+                userInput = Console.ReadLine();
+
+                if (commandCloseProgram == userInput)
+                {
+                    break;
+                }
+                else if (commandCloseProgram != userInput)
+                {
+                    tryCount++;
+                    Console.WriteLine("Попробуйте снова");
+                }
+            }
+        }
+    }
+}
